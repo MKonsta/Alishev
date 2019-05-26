61d48fb823bae19cd3c23ed8c2de91d60c817f42
MKonsta <kostyan.exe@gmail.com>
2019-05-26 13:18:51 +0600

    Dependecy injection 2

--- src/main/java/ru/konst/springcourse/MusicPlayer.java
+++ src/main/java/ru/konst/springcourse/MusicPlayer.java
@@ -3,8 +3,34 @@ package ru.konst.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    private String name; // Название мцзыкального плеера
+    private int volume; // громкость
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     // Inversion of controle
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public MusicPlayer() {
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
